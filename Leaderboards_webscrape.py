9d50b1eaf4ec7aa3ddefa6dbecd1ebb50799df38
Intern097 <john.theocharides@gmail.com>
2021-03-20 17:33:10 +0200

    Forgot to add rank 5 player and added notes

--- Leaderboards_webscrape.py
+++ Leaderboards_webscrape.py
@@ -17,24 +17,27 @@ class LOLScraper:
         #%%
         sleep(10)
         df = pd.DataFrame()
-
+        # Since top 4 players are not in the table we need to get there elements individualy
         rank_1 = driver_ranks.find_element_by_xpath('/html/body/div[3]/div[3]/div[3]/div/div/div/div[1]/ul/li[1]/a').get_attribute('href')
-        ex = {'Url 0f player': rank_1}
+        ex = {'Url of player': rank_1}
         df = df.append(ex, ignore_index=True)
         rank_2 = driver_ranks.find_element_by_xpath('/html/body/div[3]/div[3]/div[3]/div/div/div/div[1]/ul/li[2]/a').get_attribute('href')
-        ex = {'Url 0f player': rank_2}
+        ex = {'Url of player': rank_2}
         df = df.append(ex, ignore_index=True)
         rank_3 = driver_ranks.find_element_by_xpath('/html/body/div[3]/div[3]/div[3]/div/div/div/div[1]/ul/li[3]/a').get_attribute('href')
-        ex = {'Url 0f player': rank_3}
+        ex = {'Url of player': rank_3}
         df = df.append(ex, ignore_index=True)
         rank_4 = driver_ranks.find_element_by_xpath('/html/body/div[3]/div[3]/div[3]/div/div/div/div[1]/ul/li[4]/a').get_attribute('href')
-        ex = {'Url 0f player': rank_4}
+        ex = {'Url of player': rank_4}
         df = df.append(ex, ignore_index=True)
-
+        rank_4 = driver_ranks.find_element_by_xpath('/html/body/div[3]/div[3]/div[3]/div/div/div/div[1]/ul/li[5]/a').get_attribute('href')
+        ex = {'Url of player': rank_5}
+        df = df.append(ex, ignore_index=True)
+        # Main loop of getting rest of top 95 players 
         items = driver_ranks.find_elements_by_xpath("/html/body/div[3]/div[3]/div[3]/div/div/div/table/tbody/tr/td[2]/a")
         for item in items:
         
-           ex = {'Url 0f player': item.get_attribute('href')}
+           ex = {'Url of player': item.get_attribute('href')}
            df = df.append(ex, ignore_index=True)
 
         df.to_csv('top_100.csv')
